80205a507f31b5f03f55bf61bffccb37e5b525a2
dxy <duxinyu0915@outlook.com>
2024-05-31 20:31:10 +0800

    room log

--- constant.ts
+++ constant.ts
@@ -2,6 +2,6 @@ export const ERROR_MESSAGE = 'I\'m sorry, please try again later.';
 export const WECHAT_BOT_HOST = 'http://47.95.21.135:8088';
 export const AI_AGENT_HOST = 'http://47.95.21.135:8014';
 export const DB_HOST = 'http://47.95.21.135:8081';
-export const PUPPET_TOKEN = 'puppet_workpro_6675886074594f02a1eb90da2093bc2c';
+export const PUPPET_TOKEN = process.env.WECHATY_PUPPET_SERVICE_TOKEN;
 export const USE_AUDIO_REPLY = true;
 export const ROOM_NAME_LOG = '康养日志';
